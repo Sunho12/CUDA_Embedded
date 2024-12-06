9b91c86dae8d85b705f85f4432990768cd3d8dc0
Claire <113418806+Sunho12@users.noreply.github.com>
2024-12-06 12:39:07 +0900

    Add files via upload

---
+++ mmGPU_UVM/main.cu
@@ -0,0 +1,141 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cu
+ *
+ *    Description: 	Matrix Multiplication with Unified Virtual Memory
+ *
+ *        Version:  1.0
+ *        Created:  2021/07/30 10:07:38
+ *       Revision:  none
+ *       Compiler:  nvcc
+ *
+ *         Author:  Sunho Kwak, latte1210@ewhain.net
+			   Id:  2076017
+ *   Organization:  Ewha Womans University
+ *
+ * =====================================================================================
+ */
+
+#include <assert.h>
+#include "clockMeasure.h"
+
+#define checkCudaError(error) 					\
+	if(error != cudaSuccess){ 				\
+		printf("%s in %s at line %d\n", 		\
+				cudaGetErrorString(error), 	\
+				__FILE__ ,__LINE__); 		\
+		exit(EXIT_FAILURE);				\
+	}
+
+const int A_H = 512;
+const int A_W = 512;
+const int B_H = A_W;
+const int B_W = 512;
+const unsigned int MAX_NUM = 100;
+const int MAX_ITER = 10;
+
+//UVM
+unsigned int *matrixA, *matrixB, *cpuOut, *gpuOut;
+
+void generateRandomValues(unsigned int *input, const int rowSize, const int colSize){
+	for(int i = 0; i < rowSize; i++){
+		for(int j = 0; j < colSize; j++){
+			input[i * colSize + j] = (unsigned int) float(rand())/float(RAND_MAX) * MAX_NUM;
+		}
+	}
+}
+
+void printMatrixValue(const unsigned int *input, const int rowSize, const int colSize){
+	printf("Print Matrix \n -----------\n");
+	for(int i = 0; i < rowSize; i++){
+		for(int j = 0; j < colSize; j++){
+			printf("%u\t", input[i * colSize + j]);
+		}
+		printf("\n");
+	}
+	printf("--------\n");
+}
+
+bool compareMatrix(const unsigned int *inputA, const unsigned int *inputB, const int rowSize, const int colSize){
+	bool ret = true;
+	for(int i = 0; i < rowSize * colSize; i++){
+		if(inputA[i] != inputB[i]){
+			ret = false;
+			break;
+		}
+	}
+	return ret;
+}
+
+void cpuMatrixMul(const unsigned int *h_a, const unsigned int *h_b, unsigned int *h_c, const int aRowSize, const int aColSize, const int bRowSize, const int bColSize){
+	assert(aColSize == bRowSize);
+	for(int i = 0; i < aRowSize; i++){
+		for(int j = 0; j < bColSize; j++){
+			float tSum = 0.0f;
+			for(int k = 0; k < aColSize; k++){
+				tSum += (h_a[i * aColSize + k] * h_b[k * bColSize + j]);
+			}
+			h_c[i * bColSize + j] = tSum;
+		}
+	}
+}
+
+__global__
+void gpuMatrixMul(unsigned int *d_a, unsigned int *d_b, unsigned int *d_c, const int aRowSize, const int aColSize, const int bRowSize, const int bColSize){
+	int row = blockIdx.y * blockDim.y + threadIdx.y;
+	int col = blockIdx.x * blockDim.x + threadIdx.x;
+	if (row < aRowSize && col < bColSize) {
+		unsigned int sum = 0;
+		for (int i = 0; i < aColSize; i++) {
+			sum += d_a[row * aColSize + i] * d_b[i * bColSize + col];
+		}
+		d_c[row * bColSize + col] = sum;
+	}
+}
+
+int main(){
+	// Unified Memory
+	cudaMallocManaged(&matrixA, A_H * A_W * sizeof(unsigned int));
+	cudaMallocManaged(&matrixB, B_H * B_W * sizeof(unsigned int));
+	cudaMallocManaged(&cpuOut, A_H * B_W * sizeof(unsigned int));
+	cudaMallocManaged(&gpuOut, A_H * B_W * sizeof(unsigned int));
+
+	//Generating random numbers
+	srand((unsigned int)time(NULL));
+	generateRandomValues(matrixA, A_H, A_W);
+	generateRandomValues(matrixB, B_H, B_W);
+
+	cudaError_t err;
+
+	clockMeasure *ckCpu = new clockMeasure("CPU CODE");
+	ckCpu->clockReset();
+	
+	clockMeasure *ckGpu = new clockMeasure("GPU CODE");
+	ckGpu->clockReset();
+
+	dim3 blockDim(16, 16);
+	dim3 gridDim((B_W + blockDim.x - 1) / blockDim.x, (A_H + blockDim.y - 1) / blockDim.y);
+
+
+	for(int i = 0; i < MAX_ITER; i++){
+		ckCpu->clockResume();
+		cpuMatrixMul(matrixA, matrixB, cpuOut, A_H, A_W, B_H, B_W);
+		ckCpu->clockPause();
+
+		ckGpu->clockResume();
+		gpuMatrixMul<<<gridDim, blockDim>>>(matrixA, matrixB, gpuOut, A_H, A_W, B_H, B_W);
+		err=cudaDeviceSynchronize();
+		ckGpu->clockPause();
+		checkCudaError(err);
+	}
+
+	if(compareMatrix(cpuOut, gpuOut, A_H, B_W)){
+		ckCpu->clockPrint();
+		ckGpu->clockPrint();
+	}else{
+		printf("ERROR: Two Matrices are not same\n");
+	}
+
+	return 0;
+}
